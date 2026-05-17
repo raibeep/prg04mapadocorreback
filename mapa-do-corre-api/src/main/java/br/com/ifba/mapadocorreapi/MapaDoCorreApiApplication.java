fc6daf1822c8b976fd10b829d82c2b3cd484d1cc
raibeep <20242ireads0002@ifba.edu.br>
2026-05-17 09:54:45 -0300

    chore: configuração inicial do projeto com Spring Boot

---
+++ mapa-do-corre-api/src/main/java/br/com/ifba/mapadocorreapi/MapaDoCorreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba.mapadocorreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaDoCorreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaDoCorreApiApplication.class, args);
+	}
+
+}
---
+++ mapa-do-corre-api/src/test/java/br/com/ifba/mapadocorreapi/MapaDoCorreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.mapadocorreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaDoCorreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
