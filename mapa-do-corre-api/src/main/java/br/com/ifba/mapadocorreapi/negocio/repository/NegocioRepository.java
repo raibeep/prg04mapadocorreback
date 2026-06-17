be03f1013cd608cbe217cad2639f1ccee6ae7212
raibeep <20242ireads0002@ifba.edu.br>
2026-06-17 18:02:57 -0300

    feat(repository): cria repository de negocio

--- mapa-do-corre-api/src/main/java/br/com/ifba/mapadocorreapi/negocio/repository/NegocioRepository.java
+++ mapa-do-corre-api/src/main/java/br/com/ifba/mapadocorreapi/negocio/repository/NegocioRepository.java
@@ -1,4 +1,7 @@
 package br.com.ifba.mapadocorreapi.negocio.repository;
 
-public interface NegocioRepository {
+import br.com.ifba.mapadocorreapi.negocio.entity.Negocio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NegocioRepository extends JpaRepository<Negocio, Long> {
 }
