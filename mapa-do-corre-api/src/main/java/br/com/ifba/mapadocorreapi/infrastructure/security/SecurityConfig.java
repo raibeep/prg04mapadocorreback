62aa5ad606e14395013850f63ace4c6d2124df40
raibeep <20242ireads0002@ifba.edu.br>
2026-06-23 21:53:26 -0300

    fix: adiciona configuração de CORS para frontend

--- mapa-do-corre-api/src/main/java/br/com/ifba/mapadocorreapi/infrastructure/security/SecurityConfig.java
+++ mapa-do-corre-api/src/main/java/br/com/ifba/mapadocorreapi/infrastructure/security/SecurityConfig.java
@@ -49,7 +49,9 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:5173",
+                        "https://seu-front.vercel.app")
+
         );
 
         configuration.setAllowedMethods(
