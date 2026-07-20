479c3013a79d897c68045ce0322168f447bfa13d
raibeep <20242ireads0002@ifba.edu.br>
2026-07-20 10:08:25 -0300

    fix: conserta o tipo do enum

--- mapa-do-corre-api/src/main/java/br/com/ifba/mapadocorreapi/perfil/entity/Perfil.java
+++ mapa-do-corre-api/src/main/java/br/com/ifba/mapadocorreapi/perfil/entity/Perfil.java
@@ -2,7 +2,10 @@ package br.com.ifba.mapadocorreapi.perfil.entity;
 
 import br.com.ifba.mapadocorreapi.enums.TiposPerfil;
 import br.com.ifba.mapadocorreapi.infrastructure.persistence.entity.PersistenceEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Perfil extends PersistenceEntity {
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
     private TiposPerfil nivelAcesso;
 }
